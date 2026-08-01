058c244d59cc38a61e70a7eb1651f4dc2b2c2894
Alderete, Nicolás Alfredo <nicolas.alderete.2002@gmail.com>
2026-08-01 18:13:24 -0300

    FEATURE: Implement methods to convert CreateNotaRequest to Nota and NotaResponse from Nota model

--- backend/src/main/java/com/nalderete/knowledgebase/controller/DTO/request/CreateNotaRequest.java
+++ backend/src/main/java/com/nalderete/knowledgebase/controller/DTO/request/CreateNotaRequest.java
@@ -1,5 +1,7 @@
 package com.nalderete.knowledgebase.Controller.DTO.request;
 
 public record CreateNotaRequest() {
-
+    public Nota aModelo() {
+        return new Nota(null, this.titulo, this.contenido);
+    }
 }
--- backend/src/main/java/com/nalderete/knowledgebase/controller/DTO/response/NotaResponse.java
+++ backend/src/main/java/com/nalderete/knowledgebase/controller/DTO/response/NotaResponse.java
@@ -2,4 +2,12 @@ package com.nalderete.knowledgebase.Controller.DTO.response;
 
 public record NotaResponse() {
 
+    public static NotaResponse desdeModelo(Nota nota) {
+        return new NotaResponse(
+                nota.getId(),
+                nota.getTitulo(),
+                nota.getContenido()
+        );
+    }
+
 }
